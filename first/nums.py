a9c1cafe3b4ed01c81430fd571b66cf86ca7915e
Vladimir <vladimirberkutel@gmail.com>
2024-10-20 17:43:14 +0300

    init

--- first/nums.py
+++ first/nums.py
@@ -1,6 +1,14 @@
 def plus(a, b):
     return a + b
 
-if __name__ == '__main__':
-    print(plus(2,3))
 
+text = "new text"
+
+
+def minus(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    print(plus(2, 3))
+    print(minus(3, 4))
